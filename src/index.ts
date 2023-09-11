d2df152c798b311452d62535ca8566ed006c4dce
jeremy woo <jeremy>
2023-09-12 06:18:35 +1200

    minting token account.

--- src/index.ts
+++ src/index.ts
@@ -11,40 +11,109 @@ async function main() {
 
     // might need to wait for a bit until everything gets created
 
-    const mintInfo = await token.getMint(connection, mint);        
+    const mintInfo = await token.getMint(connection, mint);
+
+    console.log("mintinfo address:",mintInfo.address);
+
+    // Token Accounts are used to hold Tokens of a specific Token Mint
+    const tokenAccount = await createTokenAccount(
+        connection,
+        user,
+        mint,
+        user.publicKey
+    );
+
+    console.log("token account address:", tokenAccount.address);
+
+
+    console.log('now that you have mint and account token mint - house the mint - now you can start your minting process');
+
+    await mintTokens(
+        connection,
+        user,
+        mint,
+        tokenAccount.address,
+        user,
+        100 * 10 ** mintInfo.decimals
+    );
+
 }
-    
-    
-    async function createNewMint(
-        connection: web3.Connection,
-        payer: web3.Keypair,
-        mintAuthority: web3.PublicKey,
-        freezeAuthority: web3.PublicKey,
-        decimals: number
-        ): Promise<web3.PublicKey> {
+
+// whooohoooo - minting account 
+async function mintTokens(
+    connection: web3.Connection,
+    payer: web3.Keypair,
+    mint: web3.PublicKey,
+    destination: web3.PublicKey,
+    authority: web3.Keypair,
+    amount: number
+) {
+    const transactionSignature = await token.mintTo(
+        connection,
+        payer,
+        mint,
+        destination,
+        authority,
+        amount
+    )
+
+    console.log(
+        `Mint Token Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    )
+}
+
+
+async function createTokenAccount(
+    connection: web3.Connection,
+    payer: web3.Keypair,
+    mint: web3.PublicKey,
+    owner: web3.PublicKey
+) {
+    const tokenAccount = await token.getOrCreateAssociatedTokenAccount(
+        connection,
+        payer,
+        mint,
+        owner
+    )
+
+    console.log(
+        `Token Account: https://explorer.solana.com/address/${tokenAccount.address}?cluster=devnet`
+    )
+
+    return tokenAccount
+}
+
+
+async function createNewMint(
+    connection: web3.Connection,
+    payer: web3.Keypair,
+    mintAuthority: web3.PublicKey,
+    freezeAuthority: web3.PublicKey,
+    decimals: number
+    ): Promise<web3.PublicKey> {
+
+    const tokenMint = await token.createMint(
+        connection,
+        payer,
+        mintAuthority,
+        freezeAuthority,
+        decimals
+        );
+        
+        console.log(
+            `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
+            );
             
-            const tokenMint = await token.createMint(
-                connection,
-                payer,
-                mintAuthority,
-                freezeAuthority,
-                decimals
-                );
-                
-                console.log(
-                    `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
-                    );
-                    
-                    return tokenMint;
-                }
-                
-                main()
-                .then(() => {
-                    console.log("Finished successfully")
-                    process.exit(0)
-                })
-                .catch((error) => {
-                    console.log(error)
-                    process.exit(1)
-                })
+            return tokenMint;
+        }
+        
+        main()
+        .then(() => {
+            console.log("Finished successfully")
+            process.exit(0)
+        })
+        .catch((error) => {
+            console.log(error)
+            process.exit(1)
+        })
                 
